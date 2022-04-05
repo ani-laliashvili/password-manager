d0d31610f0814fd94a0b6b816c1b1b773e37dd7a
ani-laliashvili <71911564+ani-laliashvili@users.noreply.github.com>
2022-04-05 15:17:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def add_password():
                 file.write(f"{website.get()} | {user_name.get()} | {password.get()}\n")
                 website.delete(0, tkinter.END)
                 password.delete(0, tkinter.END)
-        except(FileNotFoundError):
+        except FileNotFoundError:
             with open("passwords.txt", mode='w') as file:
                 file.write(f"{website.get()} | {user_name.get()} | {password.get()}\n")
                 website.delete(0, tkinter.END)
